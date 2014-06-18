a55a4c1f29a350ce0cfff3b7c7eb2d4aa6625c70
Artur Malabarba <bruce.connor.am@gmail.com>
2014-06-18 23:39:20 +0100

    blabla

--- src/com/malabarba/emacsdocumentation/PersistentFragment.java
+++ src/com/malabarba/emacsdocumentation/PersistentFragment.java
@@ -4,30 +4,49 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
-class PersistentFragment extends Fragment {
+public class PersistentFragment extends Fragment {
     final public static String TAG =
         "com.malabarba.emacsdocumentation.PersistentFragment";
 
     public SectionsPagerAdapter sectionPager = null;
+
+    public PersistentFragment() {super();}
     
     static public PersistentFragment get(FragmentManager fm) {
         PersistentFragment pf = (PersistentFragment)
             fm.findFragmentByTag(PersistentFragment.TAG);
 
+        App.d("Received  PersistentFragment: " + pf);
+        
         // If the Fragment is non-null, then it is currently being
         // retained across a configuration change.
         if (pf == null) {
             pf = new PersistentFragment();
             fm.beginTransaction().add(pf, TAG).commit();
             pf.sectionPager = new SectionsPagerAdapter(fm);
-        }
+            App.d("Had to create PersistentFragment: " + pf);
+        } else if (pf.sectionPager == null) {
+            pf.sectionPager = new SectionsPagerAdapter(fm);
+            App.e("Something went wrong! The sectionPager in persistentFragment is null!");
+        } else
+            pf.sectionPager.recycle();
 
         return pf;
     }
-    
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {        
-        super.onCreate(savedInstanceState);
+    public String toString() {
+        return ""+"PersistentFragment("+sectionPager+")";
     }
 
+    @Override
+    public void onDetach() {
+        sectionPager.detach();
+        super.onDetach();
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 }
